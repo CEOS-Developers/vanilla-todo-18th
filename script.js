dd15cd2c0269a8f5650de8bbcc42cb6f93447a77
DaegyunOh <oooppq@naver.com>
2023-09-11 16:05:15 +0900

    feat: done, delete 버튼 구현
    done, delete 버튼에 onclick handler를 정의하여 클릭 이벤트에 반응할 수 있도록 했다.

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 // localStorage에서 todo elements 가져오기, 시간을 Date형식으로 변환하기 위해 map 함수를 사용
-const TODOS = JSON.parse(window.localStorage.getItem('todos')).map((todo) => ({
-  ...todo,
-  date: new Date(todo.date),
-}));
+const unparsedTODOS = localStorage.getItem('todos');
+const TODOS = unparsedTODOS
+  ? JSON.parse(unparsedTODOS).map((todo, idx) => ({
+      ...todo,
+      idx,
+      date: new Date(todo.date),
+    }))
+  : [];
+
+let nextIdx = TODOS.length;
+
+const ul = document.querySelector('.todoList');
 
 for (const todo of TODOS) {
-  const ul = document.querySelector('.todoList');
   const newLi = document.createElement('li');
   const contentDiv = document.createElement('div');
   const dateDiv = document.createElement('div');
@@ -13,18 +20,55 @@ for (const todo of TODOS) {
   const deleteBtn = document.createElement('button');
 
   contentDiv.innerHTML = todo.content;
+  contentDiv.className = 'content';
   dateDiv.innerHTML = todo.date.toISOString();
+  dateDiv.className = 'date';
   doneBtn.innerHTML = 'done';
-  doneBtn.classList;
+  doneBtn.className = 'doneBtn';
   deleteBtn.innerHTML = 'delete';
+  deleteBtn.className = 'deleteBtn';
 
   newLi.appendChild(contentDiv);
   newLi.appendChild(dateDiv);
   newLi.appendChild(doneBtn);
   newLi.appendChild(deleteBtn);
 
-  if (todo.priority === 3) newLi.classList.add = 'high';
-  else if (todo.priority === 2) newLi.classList.add = 'mid';
-  else newLi.classList.add = 'low';
+  newLi.id = todo.idx;
+  if (todo.priority === 3) newLi.classList.add('high');
+  else if (todo.priority === 2) newLi.classList.add('mid');
+  else newLi.classList.add('low');
+
+  if (todo.isDone) newLi.classList.add('done');
   ul.appendChild(newLi);
 }
+
+const doneBtns = document.querySelectorAll('.doneBtn');
+doneBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const idx = e.currentTarget.parentNode.id;
+    const clickedLi = document.getElementById(idx);
+    if (clickedLi.classList.contains('done'))
+      clickedLi.classList.remove('done');
+    else clickedLi.classList.add('done');
+
+    for (const todo of TODOS) {
+      if (todo.idx == idx) todo.isDone = !todo.isDone;
+    }
+  });
+});
+
+const deleteBtns = document.querySelectorAll('.deleteBtn');
+deleteBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const idx = e.currentTarget.parentNode.id;
+    const clickedLi = document.getElementById(idx);
+    ul.removeChild(clickedLi);
+
+    let i;
+    for (i = 0; i < TODOS.length; i++) {
+      if (TODOS[i].idx == idx) break;
+    }
+    TODOS.splice(i, 1);
+    localStorage.setItem('todos', JSON.stringify(TODOS));
+  });
+});
