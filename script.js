76e5d664404cc05e63c9a9915d982e511981e172
DaegyunOh <oooppq@naver.com>
2023-09-11 22:45:57 +0900

    chore: 주석 추가

--- script.js
+++ script.js
@@ -40,15 +40,18 @@ fromDate.addEventListener('change', handleChangeFromDate);
 // view update
 function updateView() {
   while (ul.hasChildNodes()) {
+    // 기존에 추가됐든 li 삭제
     ul.removeChild(ul.firstChild);
   }
   for (const todo of TODOS) {
+    // 업데이트된 todo list 추가
     addNewTodoLi(todo);
   }
 }
 
 // ul에 list들을 삽입
 function addNewTodoLi(todo) {
+  // li에 필요한 element들 선언
   const newLi = document.createElement('li');
   const contentDiv = document.createElement('div');
   const fromDateDiv = document.createElement('div');
@@ -56,7 +59,8 @@ function addNewTodoLi(todo) {
   const toDateDiv = document.createElement('div');
   const doneBtn = document.createElement('button');
   const deleteBtn = document.createElement('button');
-  new Date().geth;
+
+  // elements의 내용 추가
   contentDiv.innerHTML = todo.content;
   contentDiv.className = 'content';
   fromDateDiv.innerHTML = convertDate(todo.fromDate);
@@ -70,9 +74,11 @@ function addNewTodoLi(todo) {
   deleteBtn.innerHTML = '-';
   deleteBtn.className = 'deleteBtn';
 
+  // event listener 추가
   doneBtn.addEventListener('click', handleClickDoneBtn);
   deleteBtn.addEventListener('click', handleClickDeleteBtn);
 
+  // li에 child node들 추가
   newLi.appendChild(contentDiv);
   newLi.appendChild(fromDateDiv);
   newLi.appendChild(betweenDiv);
@@ -80,8 +86,9 @@ function addNewTodoLi(todo) {
   newLi.appendChild(doneBtn);
   newLi.appendChild(deleteBtn);
 
-  newLi.id = todo.idx;
-  newLi.classList.add('todoLi');
+  newLi.id = todo.idx; // 정렬을 위한 id 추가
+  // 각종 className 추가
+  newLi.classList.add('todoLi'); 
   if (todo.priority === 3) newLi.classList.add('high');
   else if (todo.priority === 2) newLi.classList.add('mid');
   else newLi.classList.add('low');
@@ -90,18 +97,18 @@ function addNewTodoLi(todo) {
   ul.appendChild(newLi);
 }
 
-// 기준에 따라 정렬되어 memory에 저장되도록 push
+// 기준에 따라 정렬되어 memory에 저장되도록 push, push할 때만 정렬하면 따로 정렬과정 거치지 않아도 됨
 function pushTodo(todo) {
-  if (todo.isDone) {
+  if (todo.isDone) { // 완료한 todo는 맨 뒤에 위치
     TODOS.splice(TODOS.length, 0, todo);
     return;
   }
   let i = 0;
   for (; i < TODOS.length; i++) {
-    if (TODOS[i].isDone) break;
-    if (TODOS[i].priority < todo.priority) break;
-    else if (TODOS[i].priority === todo.priority) {
-      if (TODOS[i].fromDate >= todo.fromDate) break;
+    if (TODOS[i].isDone) break; // 완료한 todo보다 무조건 앞에 위치
+    if (TODOS[i].priority < todo.priority) break; // priority가 더 높으면 그 앞에 위치
+    else if (TODOS[i].priority === todo.priority) { // priority가 같다면
+      if (TODOS[i].fromDate >= todo.fromDate) break; // 시작일이 빠른 todo가 더 낲에 위치
     }
   }
   TODOS.splice(i, 0, todo);
@@ -117,9 +124,11 @@ function convertDate(date) {
 function handleClickDoneBtn(e) {
   const idx = e.currentTarget.parentNode.id;
   const clickedLi = document.getElementById(idx);
+  // class 추가/제거
   if (clickedLi.classList.contains('done')) clickedLi.classList.remove('done');
   else clickedLi.classList.add('done');
 
+  // TODOS에 저장된 todo 객체 수정
   let i;
   for (i = 0; i < TODOS.length; i++) {
     if (TODOS[i].idx == idx) {
@@ -128,10 +137,10 @@ function handleClickDoneBtn(e) {
     }
   }
   const todo = TODOS[i];
-  TODOS.splice(i, 1);
 
-  pushTodo(todo);
-  updateView();
+  TODOS.splice(i, 1); // 해당 todo를 삭제한 후
+  pushTodo(todo); // 새롭게 추가(정렬을 위해)
+  updateView(); // 화면 업데이트
   localStorage.setItem('todos', JSON.stringify(TODOS));
 }
 
@@ -139,13 +148,13 @@ function handleClickDoneBtn(e) {
 function handleClickDeleteBtn(e) {
   const idx = e.currentTarget.parentNode.id;
   const clickedLi = document.getElementById(idx);
-  ul.removeChild(clickedLi);
+  ul.removeChild(clickedLi); // 클릭한 todo 삭제
 
   let i;
   for (i = 0; i < TODOS.length; i++) {
     if (TODOS[i].idx == idx) break;
   }
-  TODOS.splice(i, 1);
+  TODOS.splice(i, 1); // TODOS 에서도 삭제
 
   localStorage.setItem('todos', JSON.stringify(TODOS));
 }
@@ -153,20 +162,23 @@ function handleClickDeleteBtn(e) {
 // add button click event handler
 function handleClickAddBtn() {
   const newTodo = {};
+  // 각종 input element들 참조
   const content = document.querySelector('.contentInput');
   const priorities = document.querySelectorAll("input[name='priority']");
   const fromDate = document.querySelector('.from');
   const toDate = document.querySelector('.to');
 
+  // input들의 value값을 newTodo에 저장
   if (!content.value) return;
   newTodo.idx = nextIdx++;
   newTodo.content = content.value;
   priorities.forEach((priority) => {
     if (priority.checked) {
-      newTodo.priority = Number(priority.value);
+      newTodo.priority = Number(priority.value); // radio box에서 체크된 priority를 선택
       return;
     }
   });
+  // 에러가 발생하지 않도록 삼항연산자로 분기
   newTodo.fromDate = fromDate.value ? new Date(fromDate.value) : new Date();
   newTodo.toDate = toDate.value ? new Date(toDate.value) : new Date();
   newTodo.isDone = false;
@@ -175,6 +187,7 @@ function handleClickAddBtn() {
   updateView();
   localStorage.setItem('todos', JSON.stringify(TODOS));
 
+  // input value들을 초기화
   content.value = null;
   priorities[0].checked = true;
   fromDate.value = null;
