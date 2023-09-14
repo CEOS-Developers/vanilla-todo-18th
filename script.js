1d0ba182c5ff9334356bd2449cad288538087c88
inyoung <rmdnps10@gmail.com>
2023-09-14 15:15:51 +0900

    Feat: 로컬 스토리지에 데이터 저장 submitHandler을 통해 로컬스토리지에 input value값을 저장하고, 새로고침 시 데이터 로드

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
+let nowToDo = []; // 지금 당장 해결
+let historyToDo = []; // 해결한 내역
 const todoGenerator = document.querySelector(".todo");
 const todoList = document.querySelector(".todo-list");
-const makeToDo = (event) => {
-  event.preventDefault();
-  const todoText = event.target.children[0].value;
-  setText(todoText);
+const submitButton = document.querySelector("button");
+
+const resetInputText = () => {
+  const todo_input = document.querySelector(".todo > input");
+  console.log("d");
+  todo_input.value = "";
 };
 const setText = (text) => {
   const li = document.createElement("li");
   li.innerHTML = text;
+  nowToDo.push(text);
   todoList.appendChild(li);
 };
-todoGenerator.addEventListener("submit", makeToDo);
+const loadToDo = () => {
+  const loadToDos = localStorage.getItem("nowTodo");
+  const parsedToDos = JSON.parse(loadToDos);
+  parsedToDos.forEach((toDo) => {
+    setText(toDo);
+    console.log(toDo);
+  });
+};
+const saveToDo = () => {
+  localStorage.setItem("nowTodo", JSON.stringify(nowToDo));
+};
+const submitHandler = (event) => {
+  event.preventDefault();
+  const todoText = event.target.children[0].value;
+  setText(todoText);
+  saveToDo();
+  resetInputText();
+};
+
+todoGenerator.addEventListener("submit", submitHandler);
+
+(() => {
+  loadToDo();
+})();
