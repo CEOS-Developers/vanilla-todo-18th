a19b1642f1ca976719003bcbc1df82abff58eda6
김지원 <jibom0507@naver.com>
2023-09-14 18:35:26 +0900

    todolist update

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ function getClock() {
 }
 getClock();
 setInterval(getClock, 1000);
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
