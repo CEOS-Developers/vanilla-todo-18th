7786bda4ed8d16c7a417d8f35d54dde84c2f38b8
dhsin98 <shinjay417@gmail.com>
2023-09-15 21:03:50 +0900

    feat: 추가, 삭제 버튼 구체화

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
 const inputTodo = document.querySelector("#inputTodo");
 const todoItems = document.querySelector("#todoList");
 const doneList = document.querySelector("#doneList");
-const doneArray = document.querySelectorAll("#doneList .todoItem");
 const addBtn = document.querySelector(".addBtn");
-const todoTextArray = [];
-const doneTextArray = [];
 
+//버튼 생성하는 함수
 function createBtn(className, text) {
   var button = document.createElement("div");
-
   button.textContent = text;
-  button.setAttribute("class", "listBtns");
   button.classList.add(className);
   return button;
 }
 
+//array 삭제 후 저장하는 함수
+function deleteData(arr, value) {
+  const index = arr.indexOf(value);
+  if (index !== -1) {
+    arr.splice(index, 1);
+  }
+}
+
+//deleteBtn을 눌렀을때
 function deleteItem() {
   var removeBtns = document.querySelectorAll(".deleteBtn");
   for (i = 0; i < removeBtns.length; i++) {
     removeBtns[i].addEventListener("click", function () {
-      if (this.parentNode)
+      if (this.parentNode) {
         this.parentNode.parentNode.removeChild(this.parentNode);
+        var del = this.parentNode.firstChild.textContent;
+
+        if (todoArr.includes(del))
+          deleteData(todoArr, this.parentNode.firstChild.textContent);
+        else deleteData(doneArr, this.parentNode.firstChild.textContent);
+      }
     });
   }
 }
 
+//doneBtn을 눌렀을때
 function todoToDone() {
   var doneBtns = document.querySelectorAll(".doneBtn");
 
   for (var i = 0; i < doneBtns.length; i++) {
     doneBtns[i].addEventListener("click", function () {
-      if (this.parentNode.parentNode)
+      if (this.parentNode.parentNode === todoItems) {
         doneList.insertBefore(this.parentNode, doneList.childNodes[0]);
+        deleteData(todoArr, this.parentNode.firstChild.textContent);
+        doneArr.push(this.parentNode.firstChild.textContent);
+      }
     });
   }
 }
@@ -45,9 +60,10 @@ function newTodo() {
   newDiv.appendChild(doneBtn);
   newDiv.appendChild(deleteBtn);
   newDiv.setAttribute("class", "todoItem");
-
   todoItems.insertBefore(newDiv, todoItems.childNodes[0]);
+  todoArr.push(inputTodo.value); // todoArr 에 값 저장해줌
   inputTodo.value = "";
+
   //deleteBtn 누르면 삭제 구현
   deleteItem();
 
